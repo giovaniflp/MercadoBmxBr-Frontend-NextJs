e892ff28aae12affc48048f23c2dc75797e13ef9
giovaniflp <giovanifeitosa123@gmail.com>
2024-07-08 18:40:06 -0300

    Adicionando sistemas de ícones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 import { Button } from 'tamagui'
+import { AlertCircle } from '@tamagui/lucide-icons'
 export default function Home() {
 
-  return <Button>Hello world!</Button>
-
+  return(
+      <div>
+        <Button icon={AlertCircle} onPress={()=>{console.log("Sexo gratis")}}>Olá mundo</Button>
+      </div>
+  )
 }
